426a41c04abe60a23b4788db868a2024e40c9bab
Sean Boland <boland1919@gmail.com>
2024-02-26 23:43:07 +0000

    Outlined functions and code

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
-# testing
+from random import randint
+
+class BattleshipsGame:
+    def __init__():
+
+    
+    """
+    Prints game board
+    """
+    def print_board():
+
+    
+    """
+    Validates guess within game board
+    """
+    def validate_guess():
+
+    
+    """
+    Takes input from player and inputs it onto board
+    """
+    def make_guess():
+
+    
+    """
+    Main game functions
+    """
+    def play():
