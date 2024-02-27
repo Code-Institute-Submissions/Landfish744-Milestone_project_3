dadb1c73d01a13bd89ce075f83af7b8455fe7cae
Sean Boland <boland1919@gmail.com>
2024-02-27 22:40:01 +0000

    Add guess validation

--- run.py
+++ run.py
@@ -5,12 +5,22 @@ class BattleshipsGame:
         self.size = size
         self.num_turns = num_turns
         self.board = [['O'] * size for _ in range(size)]
-
+        self.ship_row = randint(0, size - 1)
+        self.ship_col = randint(0, size - 1)
     
     # Prints game board
     def print_board(self):
         for row in self.board:
             print(" ".join(row))
+
+
+    # Validates guess of the user
+    def validate_guess(self, row, col):
+        if 0 <= row < self.size and 0 <= col < self.size:
+            return True
+        else:
+            print("Oops, that's not even in the ocean.")
+            return False
 
     
     # Main game functions
